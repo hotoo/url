2bea19eeaeb30125315a70a337f2c8f036cb3f06
hotoo <hotoo.cn@gmail.com>
2014-02-10 15:30:34 +0800

    Add first version for url.

--- src/url.js
+++ src/url.js
@@ -1,7 +1,73 @@
 define(function(require, exports, module) {
 
-  var url;
+  var RE_URL = /^(?:(\w+:)\/\/)?((\w+):?(\w+)?@)?([^\/\?:]+):?(\d+)?(\/?[^\?#]+)?(\?[^#]+)?(#\w+)?/;
+  var DEFAULT_PORT = {
+    ftp:21,
+    ssh:22,
+    telnet:23,
+    smtp:25,
+    http:80,
+    pop3:109,
+    https:443,
+    mms:1755
+  };
 
-  module.exports = url;
+  var Url = function(url){
+    var u = RE_URL.exec(url);
+
+    this.uri = url;
+    //! URI已解码的授权组成部分，未实现。
+    this.authority;
+    this.protocol = u[1];
+    this.username = u[3];
+    this.password = u[4];
+    this.host = u[5];
+    this.port = u[6];
+    this.path = u[7];
+    this.query = u[8];
+    this._query = parseQuery(u[8]);
+    this.fragment = u[9];
+  };
+
+  function parseQuery(query){
+    var q = "?";
+    var eq = "=";
+    var and = "&";
+
+    var rst = {};
+
+    if(!query){return rst;}
+
+    query = query.replace(/^\?/, "");
+    var list = query.split(and);
+
+    for(var i=0,eqi,pair,key,val,l=list.length; i<l; i++){
+      pair = list[i];
+      eqi = pair.indexOf(eq);
+      key = decodeURIComponent(pair.substring(0, eqi)) || "";
+      val = decodeURIComponent(pair.substring(eqi+1)) || "";
+
+      if(!rst.hasOwnProperty(key)){
+        rst[key] = [val];
+      }else{
+        rst[key].push(val);
+      }
+    }
+
+    return rst;
+  }
+
+  Url.prototype.getParam = function(key){
+    return this._query.hasOwnProperty(key) ? this._query[key][0] : null;
+  };
+  Url.prototype.getParams = function(key){
+    return this._query.hasOwnProperty(key) ? this._query[key] : null;
+  };
+  Url.verify = function(uri){
+    return RE_URL.test(uri);
+  };
+
+
+  module.exports = Url;
 
 });
--- tests/url-spec.js
+++ tests/url-spec.js
@@ -1,10 +1,20 @@
 define(function(require) {
 
-  var url = require('../src/url');
+  var Url = require('url');
+  var expect = require("expect");
 
   describe('url', function() {
 
     it('normal usage', function() {
+
+      var url = new Url("http://www.google.com/path/to/index.html?q=a&u=b&q=c#hash");
+      expect(url.protocol).to.equal("http:");
+      expect(url.host).to.equal("www.google.com");
+      expect(url.path).to.equal("/path/to/index.html");
+      expect(url.query).to.equal("?q=a&u=b&q=c");
+      expect(url.getParam("q")).to.equal("a");
+      expect(url.getParam("u")).to.equal("b");
+      expect(url.getParams("q").join(", ")).to.equal("a, c");
 
     });
   });
