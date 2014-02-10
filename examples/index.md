3446b12f799af335fabbf59efe2f4badd46bcffe
hotoo <hotoo.cn@gmail.com>
2014-02-10 14:28:53 +0800

    init project

---
+++ examples/index.md
@@ -0,0 +1,9 @@
+# 演示文档
+
+---
+
+````javascript
+seajs.use('url', function(url){
+
+});
+````
---
+++ tests/url-spec.js
@@ -0,0 +1,12 @@
+define(function(require) {
+
+  var url = require('../src/url');
+
+  describe('url', function() {
+
+    it('normal usage', function() {
+
+    });
+  });
+
+});
